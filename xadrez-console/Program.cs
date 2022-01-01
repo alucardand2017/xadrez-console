9dea9667b90023f2baaa5c6e0fd50f61c98b4fce
Anderson Silva <anderson_ns5@hotmail.com>
2022-01-01 20:49:50 -0300

    criando a classe posicaoXadrez que transforma a posicao da matriz para a notacao do xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,20 +8,21 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.toPosicao());
+            //try
+            //{
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            //    tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
+            //    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            //    tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 2));
 
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 2));
-
-                Tela.imprimiTabuleiro(tab);
-            }
-            catch (TabuleiroExceptions e)
-            {
-                Console.WriteLine(e.Message); ;
-            }
+            //    Tela.imprimiTabuleiro(tab);
+            //}
+            //catch (TabuleiroExceptions e)
+            //{
+            //    Console.WriteLine(e.Message); ;
+            //}
         }
     }
 }
